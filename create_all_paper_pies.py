6e3a016f473722012c905e043f9349856874a2ed
Markus Reiter-Haas <iseratho@gmail.com>
2026-03-02 09:29:44 +0100

    Updated paper structure

--- create_all_paper_pies.py
+++ create_all_paper_pies.py
@@ -18,22 +18,24 @@ if __name__ == "__main__":
     pieviz.create_all_charts_from_dict("Rh", {"+": 75.8, "-": 23.21})
 
     # 3 Theoretical dough
+    ## 3.1 Ink efficiency
     pieviz.create_all_charts_from_csv("data/ink_efficiency.csv", prefix="Ink Efficiency: ")
+    ## 3.2 GTrends
     pieviz.create_all_charts_from_csv("data/pie_chart_trend.csv", prefix="GTrends in ")
-
-    # 4 Pieviz experiments
-
-    ## 4.1 Recursive paper analysis
-    pieviz.create_all_charts_from_dict("paper_analysis", {"blue": 69, "red": 42, "orange": 19, "green":9})
-    ## 4.2 Case study: GEB
-    pieviz.create_all_charts_from_csv("data/geb_analysis.csv", prefix="GEB: ")
-    ## 4.3 Benchmarking
-    pieviz.create_all_charts_from_dict("food101", {"apple pie": 750, "other food": 100250})
-    ## 4.4 User study
+    ## 3.3 User study
     user_pie_pieces = {f"P{k}": 1 for k in range(1, 17)} # Create a dictionary with keys 1-16 and values set to 1
     pieviz.create_all_charts_from_dict("piepieces", user_pie_pieces)
+    pieviz.create_all_charts_from_dict("identified", {"Pie Chart": 16})
     pieviz.create_all_charts_from_dict("simulation", {"Pie": 33, "Venn": 36, "UpSet": 31})
     pieviz.create_all_charts_from_dict("userstudy", {"Pie": 32.36125, "Venn": 12.07791, "UpSet": 5.68470})
+
+    # 4 Pieviz experiments
+    ## 4.1 Benchmarking
+    pieviz.create_all_charts_from_dict("food101", {"apple pie": 750, "other food": 100250})
+    ## 4.2 Case study: GEB
+    pieviz.create_all_charts_from_csv("data/geb_analysis.csv", prefix="GEB: ")
+    ## 4.3 Recursive paper analysis
+    pieviz.create_all_charts_from_dict("paper_analysis", {"blue": 69, "red": 42, "orange": 19, "green":9})
 
     # 5+ After superiority of pie charts
     pieviz.create_all_charts_from_dict("author_contribution", {"Markus": 50, "Kevin": 50})
